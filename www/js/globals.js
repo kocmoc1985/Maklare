d674da1e11a5dd92afede1e7d00307da657ae2ce
Andrei Brassas <kocmoc1985@gmail.com>
2017-04-05 08:58:57 +0200

    some small changes

--- www/js/globals.js
+++ www/js/globals.js
@@ -1,2 +1,21 @@
 var FASTIGHETS_REST = new REST('fastighet');
 var BROKERS_REST = new REST('brokers');
+
+
+function EXAMPLE_ONLY() {
+    //GET WITH OPTIONS
+    BROKERS_REST.find(_find({_fields: 'name _id', _sort: 'name', _skip: 0, _limit: 3}), function (data, textStatus, jqXHR) {
+    });
+
+    //GET ALL
+    BROKERS_REST.find('', function (data, textStatus, jqXHR) {
+    });
+
+    //GET BY ID
+    BROKERS_REST.find('588bc28d9e001a148cf713b7', function (data, textStatus, jqXHR) {
+    });
+
+    //GET BY SOME PARAM
+    BROKERS_REST.find(_find({name: 'John Doe'}), function (data, textStatus, jqXHR) {
+    });
+}
