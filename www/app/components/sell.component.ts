e75b231561d4bad9af08f61a08e024dc06fdff20
Pelle Härdstedt <pelle.hardstedt@gmail.com>
2017-04-06 13:33:29 +0200

    Content and styling

--- www/app/components/sell.component.ts
+++ www/app/components/sell.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'sell',
-    templateUrl: 'app/components/template/sell.html'
+    templateUrl: 'app/components/template/sell.html',
+    styleUrls: ['app/components/css/sell.css']
 })
 
 export class SellComponent {
