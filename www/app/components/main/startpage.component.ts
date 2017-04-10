e719874b4c6f65ed22ba6ae2a383225757029df8
Pelle Härdstedt <pelle.hardstedt@gmail.com>
2017-04-10 09:55:48 +0200

    Router linking from and to start page

--- www/app/components/main/startpage.component.ts
+++ www/app/components/main/startpage.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-startpage',
-  templateUrl: 'app/components/template/startpage.html'
+    selector: 'app-startpage',
+    templateUrl: 'app/components/template/startpage.html',
+    styleUrls: ['app/components/css/startpage.css']
+
 })
 
 export class StartpageComponent {
