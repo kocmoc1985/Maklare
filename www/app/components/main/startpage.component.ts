8015af9eadf4346227304da7b9a964fcb5cedf62
Pelle Härdstedt <pelle.hardstedt@gmail.com>
2017-04-10 10:11:17 +0200

    Two start pages

--- www/app/components/main/startpage.component.ts
+++ www/app/components/main/startpage.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-startpage',
-    templateUrl: 'app/components/template/startpage.html',
-    styleUrls: ['app/components/css/startpage.css']
+  selector: 'app-startpage',
+  templateUrl: 'app/components/template/startpage-pelle.html',
+  // templateUrl: 'app/components/template/startpage.html',
+  styleUrls:['app/components/css/startpage.css']
 
 })
 
