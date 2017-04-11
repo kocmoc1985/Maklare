a82b601b73188efa1a50f75d300a18673e2bc63a
David <david.bottiger@gmail.com>
2017-04-11 10:19:49 +0200

    Last commit was wrong sry, fixed it

--- www/app/components/main/sell.component.ts
+++ www/app/components/main/sell.component.ts
@@ -1,23 +1,25 @@
 import { Component } from '@angular/core';
+import {Http} from "@angular/http";
 
 @Component({
     selector: 'sell',
     templateUrl: 'app/components/template/sell.html',
-    styleUrls: ['app/components/css/sell.css']
+    styleUrls: ['app/components/css/sell.css'],
 })
 
 export class SellComponent {
-		email: string;
+
+	email: string;
 	phoneNumber: string;
 	sent: boolean = false;
 
 	constructor(private http: Http) {}
 
-	//Send mailadress to mail
+	//send mail
 	registerMail(){
 		this.sendContactInfo({email:this.email});
 	}
-	//Send phonenumber to mail
+	//send phonenumber
 	registerPhone(){
 		this.sendContactInfo({phoneNumber:this.phoneNumber});
 	}
@@ -27,7 +29,6 @@ export class SellComponent {
 		let _observable = this.http.post("/register-contact-info", data);
 		_observable.subscribe();
 		console.log("SHOULD SEND",data,"HA",this.http);
-
 	}
 
 
